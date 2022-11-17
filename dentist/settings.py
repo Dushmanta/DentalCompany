05b84c4a0a5796c55525c73557c4f87db5d1ae0d
Dushmanta <dushmanta.kiit@gmail.com>
2022-11-17 09:41:13 +0530

    fix

--- dentist/settings.py
+++ dentist/settings.py
@@ -120,7 +120,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
